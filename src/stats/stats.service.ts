62d1a4a63b6c8079ba7a6499a7883068dba4ff07
Matt Raffel <mattr@kiva.org>
2022-04-27 08:48:19 -0600

    updating for various services
    Signed-off-by: Matt Raffel <mattr@kiva.org>

--- src/stats/stats.service.ts
+++ src/stats/stats.service.ts
@@ -1,10 +1,11 @@
 import { HttpService, Injectable } from '@nestjs/common';
+import { Constants } from 'protocol-common/constants';
 import { ProtocolHttpService } from 'protocol-common/protocol.http.service';
 import { Logger } from 'protocol-common/logger';
 import { SummaryReportDto } from './dtos/summary.report.dto';
 import { ServiceReportDto } from './dtos/service.report.dto';
 import { AppService } from '../app/app.service';
-import { hosts } from '../config/services.json';
+import servicesData from '../config/services.json';
 
 
 @Injectable()
@@ -17,33 +18,61 @@ export class StatsService {
 
     public async generateReport(): Promise<SummaryReportDto> {
 
-        Logger.info(`System statistics report generated`);
+        Logger.info('System statistics report generated');
 
         const report: SummaryReportDto = new SummaryReportDto();
         report.reportDate = new Date().toDateString();
         report.reportingServices.push(this.getThisServiceReport());
 
-
+        const hosts : any = this.getHosts();
+        Logger.warn(`got back ${JSON.stringify(hosts)}`);
         for(const serviceName of hosts)  {
             try {
                 // for performance it might be nice to not wait for each service
-                // and let each one reply syncronously
+                // and let each one reply synchronously
                 const details: ServiceReportDto = await this.getServiceReport(serviceName);
+                // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
                 Logger.log(`query ${serviceName} returned`, details);
                 report.reportingServices.push(details);
             } catch(e) {
+                // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
                 Logger.error(`${serviceName} failed to provide stats. ${e.message}`, e);
                 // any error indicates the service was not functional
                 report.failedServices.push(serviceName);
             }
         }
-        
+
         return Promise.resolve(report);
+    }
+
+    private getHosts(): any {
+        let dataSet : any;
+        switch (process.env.NODE_ENV) {
+            case Constants.PROD:
+                dataSet = {...servicesData.prod};
+                break;
+            case Constants.SAND:
+                dataSet = {...servicesData.sand};
+                break;
+            case Constants.QA:
+                dataSet = {...servicesData.qa};
+                break;
+            case Constants.DEV:
+                dataSet = {...servicesData.dev};
+                break;
+            case Constants.LOCAL:
+                dataSet = {...servicesData.local};
+                break;
+            default:
+                throw new Error(`NODE_ENV ${process.env.NODE_ENV} is not a valid value`);
+        }
+
+        return dataSet.hosts;
     }
 
     private getThisServiceReport(): ServiceReportDto {
         const service = new ServiceReportDto();
-        service.serviceName = process.env.SERVICE_NAME
+        service.serviceName = process.env.SERVICE_NAME;
         service.startedAt = AppService.getStartDate().toDateString();
         service.currentTime = new Date().toDateString();
         // TODO: finish
@@ -58,6 +87,7 @@ export class StatsService {
         };
         const result = await this.http.requestWithRetry(req);
         Logger.info(`${serviceName} returned `, result.data);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
         return Promise.resolve( result.data );
     }
 }
