8f111fee3b453ad8733b7b4db7821ccb6d04ee6e
Matt Raffel <mattr@kiva.org>
2022-06-02 12:37:16 -0600

    fix lint
    Signed-off-by: Matt Raffel <mattr@kiva.org>

--- src/stats/stats.module.ts
+++ src/stats/stats.module.ts
@@ -1,5 +1,5 @@
 import { Module, HttpModule } from '@nestjs/common';
-import { ProtocolHttpModule } from "protocol-common";
+import { ProtocolHttpModule } from 'protocol-common';
 import { StatsController } from './stats.controller.js';
 import { StatsService } from './stats.service.js';
 import { AppController } from '../app/app.controller.js';
