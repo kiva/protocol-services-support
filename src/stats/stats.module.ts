66a0a581f0c17212dd2d94f68adab35dd7b68c0d
Matt <52470716+matt-raffel-kiva@users.noreply.github.com>
2022-06-02 12:50:14 -0600

    fixing build (#9)
    * fixing build
    
    Signed-off-by: Matt Raffel <mattr@kiva.org>
    
    * fix lint
    
    Signed-off-by: Matt Raffel <mattr@kiva.org>

--- src/stats/stats.module.ts
+++ src/stats/stats.module.ts
@@ -1,10 +1,11 @@
 import { Module, HttpModule } from '@nestjs/common';
+import { ProtocolHttpModule } from 'protocol-common';
 import { StatsController } from './stats.controller.js';
 import { StatsService } from './stats.service.js';
 import { AppController } from '../app/app.controller.js';
 
 @Module({
-    imports: [HttpModule],
+    imports: [HttpModule, ProtocolHttpModule],
     controllers: [StatsController, AppController],
     providers: [StatsService],
 })
